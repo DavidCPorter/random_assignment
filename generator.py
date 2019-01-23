b6c46538f37f33be89b6a62bf9568a74088573ce
David Porter <david@claimdocs.com>
2019-01-23 16:45:35 -0600

    done

--- generator.py
+++ generator.py
@@ -5,15 +5,12 @@ from random import shuffle
 
 # def main():
 dt = pd.read_csv('paper_reviews.csv', index_col=0).to_dict()
-# students =
-# print(dt)
-# get a master list of the papers and add them as keys to a dict
-
-# preprocess
-# student
 
 # for each student, look if they selected paper
 papersToStudents = {el:[] for el in master.split(';')}
+
+
+print('\n \n \n \n', "TOTAL NUMBER OF PAPERS ON THE LIST -> ", len(papersToStudents), '\n \n \n \n')
 
 studentsToPreferredPapers = dict()
 studentsToPapers = dict()
@@ -29,6 +26,7 @@ for i in dt['papers']:
 randomPapers=list(papersToStudents.keys())
 shuffle(randomPapers)
 
+#give preference to students that submitted forms
 for paper in randomPapers:
     for student in studentsToPreferredPapers:
     #iterate through the papers to see if the students chose, then add the student to the papersToStudents.
@@ -40,15 +38,25 @@ for paper in randomPapers:
         except:
             pass
 
+studentsLazy = []
+for i in range(0,15):
+    studentsLazy.append("student_"+str(i))
+    studentsToPapers["student_"+str(i)]=[]
+
+
+#students that did not submit will be assigned to the front of the reading list.
+for paper in list(papersToStudents.keys()):
+    for student in studentsLazy:
+        if len(papersToStudents[paper])<4 and len(studentsToPapers[student])<8:
+            papersToStudents[paper].append(student)
+            studentsToPapers[student].append(paper)
+
+print('\n\n', 'MAPPING OF STUDENTS -> PAPER:','\n\n\n')
+
 for student in studentsToPapers:
-    print(student, '-->', studentsToPapers[student], '\n')
+    print(student, '\n-->', str(len(studentsToPapers[student]))+' assigned papers:', '\n', studentsToPapers[student],'\n')
 
+print('\n\n', 'MAPPING OF PAPER -> STUDENTS:','\n\n')
 
-
-
-
-# print(studentsMap['Sidant Pani']['Architecture: Storage The Hadoop Distributed File System, Schvachko et al, MSST, 2010.'])
-
-
-# if __name__ == '__main__':
-#     main()
+for paper in papersToStudents:
+    print(paper, '\n-->',str(len(papersToStudents[paper]))+' assigned students:', '\n', papersToStudents[paper],'\n')
